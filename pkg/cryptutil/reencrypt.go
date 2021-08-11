48a6ac368e360a96261c8158d4015b42476548a0
Kim Tore Jensen <kimtjen@gmail.com>
2021-08-11 16:13:35 +0200

    strip whitespace when re-encrypting variables

--- pkg/cryptutil/reencrypt.go
+++ pkg/cryptutil/reencrypt.go
@@ -1,11 +1,15 @@
 package cryptutil
 
+import (
+	"strings"
+)
+
 // Decrypt data encrypted with OpenSSL, and re-encrypt with secure parameters.
 func ReEncrypt(ciphertext, password string) (string, error) {
 	plaintext, err := DecryptOpenSSL(ciphertext, password)
 	if err != nil {
 		return "", err
 	}
-
+	plaintext = strings.TrimSpace(plaintext)
 	return EncryptWithPassword(plaintext, password)
 }
